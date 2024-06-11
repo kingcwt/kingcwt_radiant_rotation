db0db7f3d04dc45a20b308f9aad2449625c90c1e
kingcwt <kingcwt@qq.com>
2024-06-11 22:58:26 +0800

    feat: 调整阅读时间

--- remark-reading-time.mjs
+++ remark-reading-time.mjs
@@ -6,6 +6,7 @@ export function remarkReadingTime() {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
     // readingTime.text 会以友好的字符串形式给出阅读时间，例如 "3 min read"。
-    data.astro.frontmatter.minutesRead = Math.floor(readingTime.minutes);
+    console.log(readingTime.minutes, '时间')
+    data.astro.frontmatter.minutesRead = Math.ceil(readingTime.minutes);
   };
 }
